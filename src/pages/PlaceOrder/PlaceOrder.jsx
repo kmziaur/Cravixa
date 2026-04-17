6101f6142b476fb034869f35855cc1ac094dad33
Ziaur Rahman <cse1905008brur@gmail.com>
2026-04-18 00:11:44 +0600

    5th update

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -15,7 +15,7 @@ const PlaceOrder = () => {
           <input type="text" placeholder="Last name" />
         </div>
 
-        <input type="email" placeholder="Email address" />
+        <input type="email" placeholder="Email address" required />
         <input type="text" placeholder="Street" />
 
         <div className="multi-fields">
@@ -23,10 +23,10 @@ const PlaceOrder = () => {
           <input type="text" placeholder="State" />
         </div>
         <div className="multi-fields">
-          <input type="text" placeholder="Zip code" />
+          <input type="text" placeholder="Zip code" required />
           <input type="text" placeholder="Country" />
         </div>
-        <input type="text" placeholder="Phone" />
+        <input type="text" placeholder="Phone" required />
       </div>
 
       <div className="place-order-right">
@@ -34,18 +34,18 @@ const PlaceOrder = () => {
           <h2>Cart Totals</h2>
           <div>
             <div className="cart-total-details">
-              <p>Subtotal</p>
+              <p>Total Product Price</p>
               <p>${getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${2}</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount() + 2}</b>
+              <b>${getTotalCartAmount()===0?0:getTotalCartAmount() + 2}</b>
             </div>
           </div>
           <button>PROCEED TO PAYMENT</button>
