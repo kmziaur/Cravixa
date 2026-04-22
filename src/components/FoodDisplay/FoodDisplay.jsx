92e412bceecb13d46dd825c4eb828864f8b87de1
Ziaur Rahman <cse1905008brur@gmail.com>
2026-04-22 23:52:55 +0600

    6th update

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -4,7 +4,7 @@ import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
 const FoodDisplay = ({ category }) => {
-  const { food_list } = useContext(StoreContext);
+  const { food_list, url } = useContext(StoreContext);
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
@@ -12,7 +12,7 @@ const FoodDisplay = ({ category }) => {
         {food_list.map((item, index) => {
           if(category === "All" || category===item.category)
           {
-           return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+           return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.Image} />
           }
         })}
       </div>
